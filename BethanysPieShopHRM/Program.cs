db3046c514008b0391aa41246446e3cdaa04e7be
Rafael Teles <rafaeltelesalbertonii@gmail.com>
2023-03-29 16:16:05 -0300

    Classes created

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BethanysPieShopHRM
+{
+    public class BethanysPieShopHRM
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bethany's Pie Show Emplyee App\n");
+
+            Utilities.CheckForExistingEmployeesFile();
+            string userSelect;
+            do
+            {
+                Console.WriteLine("Select an action:");
+                Console.WriteLine("1: Register employee");
+                Console.WriteLine("2: View all employee");
+                Console.WriteLine("3: Save data");
+                Console.WriteLine("4: Load data");
+                Console.WriteLine("9: Quit application");
+                Console.Write("Your Selection: ");
+                
+                userSelect = Console.ReadLine();
+                switch (userSelect) 
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "9":
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }while (userSelect != "9") ;
+
+            Console.WriteLine("Thanks for using the application");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ BethanysPieShopHRM/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+    internal class Utilities
+    {
+		private static string directory = @"C:\Projetos\BethanysPieShopHRM\";
+		private static string fileName = "employees.txt";
+
+		internal static void CheckForExistingEmployeesFile()
+		{
+			string path = $"{directory}{fileName}";
+			bool existingFileFound = File.Exists(path);
+
+			if(existingFileFound)
+			{
+				Console.ForegroundColor = ConsoleColor.Blue;
+				Console.WriteLine("An existing file with employee data is found!\n");
+				Console.ResetColor();
+			}
+            else
+            {
+				if (!Directory.Exists(directory))
+                {
+					Directory.CreateDirectory(directory);
+					Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Directory ready for saving files!\n");
+					Console.ResetColor();
+                }
+            }
+		}
+
+		internal static void RegisterEmployee()
+        {
+
+        }
+
+
+
+	}
+}
